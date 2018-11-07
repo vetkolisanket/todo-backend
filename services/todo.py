1464606010948d7807fe4933aef952958e13c2b3
Sanket <vetkolisanket@gmail.com>
2018-11-07 12:54:59 +0530

    Initial commit

---
+++ services/todo.py
@@ -0,0 +1,45 @@
+import json
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+database_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+print database_path
+
+
+with open("{}/database/todo.json".format(database_path), "r") as jsf:
+    todo_list = json.load(jsf)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    '''  Greet the user '''
+
+    return "Todo service is up"
+
+
+@app.route('/lists', methods=['GET'])
+def show_lists():
+    '''  Display all the lists '''
+
+    tlists = []
+    for username in todo_list:
+        for lname in todo_list[username]:
+            tlists.append(lname)
+    return jsonify(lists=tlists)
+
+
+@app.route('/lists/<username>', methods=['GET'])
+def user_list(username):
+    '''  Returns a user oriented list '''
+
+    if username not in todo_list:
+        return "No list found"
+
+    return jsonify(todo_list[username])
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
