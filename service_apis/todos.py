9e9afb4f8a83241f2748799c5f688a89bedd12e4
Sanket <vetkolisanket@gmail.com>
2019-04-30 20:45:54 +0530

    Put call wip

--- service_apis/todos.py
+++ service_apis/todos.py
@@ -5,6 +5,7 @@ from flask import request, abort, jsonify
 
 from models.restresponse import RestResponse
 from models.todos import Todo
+from utils.utils import error_response
 
 
 class Todos(Resource):
@@ -20,7 +21,20 @@ class Todos(Resource):
 
     def post(self):
         if not request.json:
-            abort(400)
+            error_response(400, 'Body required!')
         todo = request.json
         todo = Todo(description=todo['description'], status=todo['status'])
         return todo.get_dict()
+
+    def put(self, todo_id):
+        if not todo_id:
+            error_response(400, 'id not found')
+        if not request.json:
+            error_response(400, 'Body required!')
+        body = request.json        # validate json body
+        for todo in Todo.todos:
+            if todo['id'] == todo_id:
+                todo['description'] = body['description']
+                todo['status'] = body['status']
+                return todo
+        error_response(404, 'Not Found!')
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,25 @@
+from flask import Response
+
 headers_mapping = {'csv': {'content-type': 'application/csv'},
                    'json': {'content-type': 'application/json'}}
 
 
-def ok_response(response, status=False, message="OK", headers='json', code=200):
-    return status, response, code, message, headers_mapping[headers]
+# def ok_response(response, status=False, message="OK", headers='json', code=200):
+#     return status, response, code, message, headers_mapping[headers]
+
+
+def ok_response(response, status=True, message="OK", headers='json', code=200):
+    return {
+        'responseData': response,
+        'status': status,
+        'message': message
+    }
+
+
+def error_response(code, message, response=None, status=False , headers='json'):
+    res = {
+        'responseData': response,
+        'status': status,
+        'message': message
+    }
+    return Response(res, status, mimetype='application/json')
